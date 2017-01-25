46a4497be6049a9a5a88c3f586e95ff75eaddf51
FrancescoUchiha88 <francesco.a.lucci@gmail.com>
2017-01-25 17:36:24 +0100

    Version A02.1
    Right precision for character creation

--- MangaToPDF/src/main/java/com/myproject/engine/MangaEngine.java
+++ MangaToPDF/src/main/java/com/myproject/engine/MangaEngine.java
@@ -5,19 +5,13 @@
  */
 package com.myproject.engine;
 
-import com.itextpdf.text.BadElementException;
 import com.itextpdf.text.Chapter;
 import java.io.FileOutputStream;
 import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.PageSize;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import static java.util.Arrays.stream;
-import javax.activation.MimetypesFileTypeMap;
 import javax.imageio.ImageIO;
 
 /**
@@ -26,17 +20,20 @@ import javax.imageio.ImageIO;
  */
 public class MangaEngine {
 
+    private static int cap;
+    
     public static void engine(String pathManga, String pathPDF, String namePDF) {
 
         try {
             File f = new File(pathManga);
+            cap=0;
             Document document = new Document(PageSize.A4, 50, 50, 50, 50);
             String output = pathPDF + "//" + namePDF + ".pdf";
             FileOutputStream fos = new FileOutputStream(output);
             PdfWriter writer = PdfWriter.getInstance(document, fos);
             writer.open();
             document.open();
-            scorriAlbero(f, document, true, 0);
+            scorriAlbero(f, document, true);
             document.addAuthor("Francesco Lucci");
             document.addTitle(namePDF);
             document.addCreationDate();
@@ -47,7 +44,7 @@ public class MangaEngine {
         }
     }
 
-    private static void scorriAlbero(File f, Document document, boolean isFirst, int cap) {
+    private static void scorriAlbero(File f, Document document, boolean isFirst) {
         File[] lista = f.listFiles();
         try {
             for (int i = 0; i < lista.length; i++) {
@@ -55,19 +52,19 @@ public class MangaEngine {
                     if (ImageIO.read(lista[i]) != null) {
                         if (isFirst) {
                             cap=cap+1;
-                            Chapter chapter = new Chapter(lista[i].getPath().split("\\\\")[lista[i].getPath().split("\\\\").length - 2], 1);
-                            System.out.println("----------------CAPITOLO: " + cap + " - " +lista[i].getPath().split("\\\\")[lista[i].getPath().split("\\\\").length - 2] + "----------------------");
+                            Chapter chapter = new Chapter(" - " + lista[i].getPath().split("\\\\")[lista[i].getPath().split("\\\\").length - 2], 1);
+                            //System.out.println("----------------CAPITOLO: " + cap + " - " +lista[i].getPath().split("\\\\")[lista[i].getPath().split("\\\\").length - 2] + "----------------------");
                             document.add(chapter);
                             isFirst = false;
                         }
-                        System.out.println(lista[i].getPath());
+                        //System.out.println(lista[i].getPath());
                         Image image = Image.getInstance(lista[i].getPath());
                         image.scaleAbsolute(550f, 770f);
                         document.add(image);
                     }
                 } else {
-                    System.out.println(lista[i].getPath());
-                    scorriAlbero(lista[i], document, !isFirst, cap);
+                    //System.out.println(lista[i].getPath());
+                    scorriAlbero(lista[i], document, !isFirst);
                 }
             }
         } catch (Exception e) {
